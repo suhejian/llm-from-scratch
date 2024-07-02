50553cd6e97ba1d27d200a5f68306967f47efa50
suhejian <hejiansu@126.com>
2024-07-02 15:17:08 +0800

    Update spm_train_tokenizer.py

--- train-tokenizer/spm_train_tokenizer.py
+++ train-tokenizer/spm_train_tokenizer.py
@@ -27,5 +27,26 @@ def train_tokenizer(corpus_file, model_name="tiny-llm"):
     print(f"Finish Training, Cost: {end_time - start_time}s")
 
 
+def use_tokenizer(text, model_path):
+    # makes segment instance and loads the model file
+    sp = spm.SentencePieceProcessor()
+    sp.load(model_path)
+
+    # encode: text ---> ids
+    token_pieces = sp.encode_as_pieces(text)
+    token_ids = sp.encode_as_ids(text)
+    print(f"Raw Text: {text}")
+    print(f"Token pieces: {token_pieces}")
+    print(f"Token ids: {token_ids}")
+    print(len(token_ids))
+    # decode: ids ---> text
+    print(sp.decode_pieces(token_pieces))
+    print(sp.decode_ids(token_ids))
+
+
 if __name__ == "__main__":
-    train_tokenizer(corpus_file="/root/autodl-tmp/Corpus/corpus_for_train_tokenizer.txt", model_name="tiny-llm")
+    # train_tokenizer(corpus_file="/root/autodl-tmp/Corpus/corpus_for_train_tokenizer.txt", model_name="tiny-llm")
+
+    model_path = "./save_models/tiny-llm.model"
+    text = "苟利国家生死以，岂因祸福避趋之"
+    use_tokenizer(text, model_path)
